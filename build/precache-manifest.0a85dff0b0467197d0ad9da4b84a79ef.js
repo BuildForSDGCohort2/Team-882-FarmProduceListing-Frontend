6212be95ef087d85c6e54c4ee9170e7e314ca24e
Austaino <c3ono32@yahoo.com.com>
2020-10-09 13:51:59 -0700

    deploying

---
+++ build/precache-manifest.0a85dff0b0467197d0ad9da4b84a79ef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef0e37e3f43f3d3003e013afc418523",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd1ce8958d6739519dc5",
+    "url": "/static/css/main.6d1439c2.chunk.css"
+  },
+  {
+    "revision": "86cbcb7d8f26065de13e",
+    "url": "/static/js/2.cebf9fc9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cebf9fc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd1ce8958d6739519dc5",
+    "url": "/static/js/main.e1d6f015.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "3cec182ff0819f82d458541dcad0bbd7",
+    "url": "/static/media/banner.3cec182f.png"
+  },
+  {
+    "revision": "1617c9f803b9c0a5b645911475f109bc",
+    "url": "/static/media/navbarnow.1617c9f8.png"
+  },
+  {
+    "revision": "bb7df16a8a73e39481808ceb626cb26d",
+    "url": "/static/media/produce.bb7df16a.jpeg"
+  }
+]);
